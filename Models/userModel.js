4b7ec06399adac4d01237110099a39f7d1a27e87
BurcuHaddad <burcuhaddad@gmail.com>
2023-03-24 12:00:19 +0300

    user model

---
+++ Models/userModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  role: {
+    type: String,
+    enum: ["admin", "seller"],
+    default: "seller",
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
+  },
+});
--- index.js
+++ index.js
@@ -12,7 +12,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/", {
     useUnifiedTopology: true
 }, console.log("Connected"))
 
+app.get("/", (req, res) => {
+    res.send("Hello from the server side")
+})
+
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
+
