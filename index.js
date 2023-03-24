817b166104c9a055b4c29986cc5aaca08028e806
BurcuHaddad <burcuhaddad@gmail.com>
2023-03-24 09:02:19 +0300

    db connection

---
+++ index.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const dotenv = require("dotenv");
+
+const app = express();
+
+const DB = "mongodb://localhost:27017/crm";
+
+mongoose.connect("mongodb://127.0.0.1:27017/", {
+    dbName: "crm",
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, console.log("Connected"))
+
+const port = process.env.PORT || 5000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
